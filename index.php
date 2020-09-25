d2c40282fd2467fca1b34c7c27f98bba759a62ec
danielbuiles <61318415+danielbuiles@users.noreply.github.com>
2020-09-25 12:12:57 -0500

    Planilla
    Acabo de realizar toda la mayor parte de html de la pagina solo me faltarian unos formularios y el codigo del php.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/styles.css">
+	<title>Pagina Taller web 1</title>
+</head>
+<body>
+	<div class="container">
+		<div class="section">
+			<div class="content">
+				<h2>EJERCICIO N°1</h2>
+			</div>
+		</div>
+		<div class="section">
+			<div class="content">
+				<h2>EJERCICIO N°2</h2>
+			</div>
+		</div>
+		<div class="section">
+			<div class="content">
+				<h2>EJERCICIO N°4</h2>
+			</div>
+		</div>
+		<div class="section">
+			<div class="content">
+				<h2>EJERCICIO N°6</h2>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+	</footer>
+</body>
+</html>
